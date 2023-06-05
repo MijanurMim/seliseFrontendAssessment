f3fda7edabc12a495b6b967f2aa5930e85f9f592
MijanurMim <mijanurmim1@gamail.com>
2023-06-05 17:47:47 +0600

    [DONE] Assessment done

--- src/components/bookmark/Bookmark.js
+++ src/components/bookmark/Bookmark.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+
 import Category from "../category/Category";
 
 const Bookmark = () => {
--- src/components/category/CategoryItems.js
+++ src/components/category/CategoryItems.js
@@ -6,7 +6,7 @@ const CategoryItems = ({ data }) => {
       <li>{data.title} </li>
       <li className="mt-1">
         {" "}
-        <button class="rounded-sm bg-gray-200  p-2 border-gray-600 hover:bg-gray-300 duration-200">
+        <button className="rounded-sm bg-gray-200  p-2 border-gray-600 hover:bg-gray-300 duration-200">
           Details
         </button>
       </li>
